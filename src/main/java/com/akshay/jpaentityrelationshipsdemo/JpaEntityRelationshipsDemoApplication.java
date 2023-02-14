f3de01d89eab3a3e93679ba95a60657d849c17e0
coding-streams <akxhdev@outlook.com>
2023-02-14 15:49:08 +0530

    initial commit

---
+++ src/main/java/com/akshay/jpaentityrelationshipsdemo/JpaEntityRelationshipsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.jpaentityrelationshipsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaEntityRelationshipsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaEntityRelationshipsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshay/jpaentityrelationshipsdemo/JpaEntityRelationshipsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.jpaentityrelationshipsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaEntityRelationshipsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
